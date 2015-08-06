56877458ef3853e25871e8747cdd8524a86ec2ba
David Oliver <david.oliver@codehabit.net>
2015-08-06 13:09:27 -0500

    Removing irrelevant specs from computer player spec

--- spec/lib/computer_player_spec.rb
+++ spec/lib/computer_player_spec.rb
@@ -9,25 +9,5 @@ describe ComputerPlayer do
     subject.marker = "foo"
     expect(subject.marker).to eq "foo"
   end
-
-  describe "the computer player's turn" do
-    let(:game) do
-      game = TicTacToe.new
-      game.external_player = Player.new
-      game
-    end
-
-    it "knows it can win on the next move" do
-      computer_player = game.computer_player
-      expect(game).to receive(:winning_moves).with(computer_player.marker).and_return [[0,0]]
-      computer_player.move
-    end
-
-    it "knows it can win on the next move" do
-      computer_player = game.computer_player
-      expect(game).to receive(:winning_moves).with(game.external_player.marker).and_return [[0,0]]
-      computer_player.move
-    end
-  end
 end
 
